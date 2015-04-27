06032cd4c153c22f0039bc086c081efcd73f0fe7
Gorunov Victor <vforumreg2015@mail.ru>
2015-04-27 22:56:09 +0400

    Выполнено задание № 1. Урок 3.

--- index.php
+++ index.php
@@ -1,55 +1,69 @@
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-<h1>Задание № 1</h1>
+<h1>Урок 3. Задание № 1</h1>
 <?php
-//    header('Content-Type: text/html; charset=utf-8'); //иначе кракозябры
+    /*
+ * - Создайте массив $date с пятью элементами
+ * - C помощью генератора случайных чисел забейте массив $date юниксовыми метками
+ *
+ */
+    $date = array();
+    //получим текущее количество секунд
+    $timestamp_now = time();
+    for ($i = 0; $i < 4; $i++)
+    {
+        array_push($date, mt_rand(1, $timestamp_now));
+    }
+    echo "Исходное состояние:";
+    var_dump($date);
 
-    $name = 'Victor';
-    $age = '34';
+    /*
+     * - Сделайте вывод сообщения на экран о том, какой день в сгенерированном массиве получился наименьшим, а какой месяц
+     * наибольшим
+     *
+     */
+    $date_normal = array();
+    $min_day = 32;
+    $max_month = 0;
 
-    //\r\n или \n не работают. Если не ставить хедер, идут кракозябры
-    echo "Меня зовут ".$name.".<br>Мне ".$age." года.";
+    foreach ($date as $value) {
+         //не знаю, что быстрее, один раз объявить, потом использовать, либо в условии
+        //второй раз использовать date()
+        $cur_day = date('j',$value);
+        $cur_month = date('n',$value);
+//        echo "Тек. день: ".$cur_day." Мин. день:".$min_day."<br>";
+//        echo "Тек. месяц: ".$cur_month." Макс. месяц:".$max_month."<br><br>";
 
-    unset($name, $age);
-?>
-<h1>Задание № 2</h1>
-<?
-    define("CITY", 'Samara');
-    if (defined('CITY')) echo 'Константа определена: '.constant('CITY');
-    else echo 'Константа НЕ определена';
+        if ($cur_day < $min_day)
+                $min_day = $cur_day;
 
-    echo "<br>".CITY;
-    define("CITY", 'Moscow');
+        if ($cur_month > $max_month)
+                $max_month = $cur_month;
 
-?>
+        array_push($date_normal, date('d-m-Y',$value));
+    }
 
-<h1>Задание № 3</h1>
-<?
-$book = [
-    "author" => 'Дольник',
-    "title" => 'Непослушное дитя биосферы',
-    "pages" => 384
-];
-//Недавно я прочитал книгу 'title', написанную автором author, я осилил все pages страниц, мне она очень понравилась"
-echo "Недавно я прочитал книгу \"".$book['title']."\", написанную автором ".$book['author'].", я осилил все ".$book['pages']." страниц, мне она очень понравилась"
-?>
-<h1>Задание № 4</h1>
-<?
+    echo "Даты в привычном представлении:";
+    var_dump($date_normal);
+    echo "Наименьший день:".$min_day;
+    echo "<br>Наибольший месяц:".$max_month;
 
-$books = [
-    array(
-        "author" => 'Дольник',
-        "title" => 'Непослушное дитя биосферы',
-        "pages" => 384
-    ),
-    array(
-    "author" => 'Дилтс',
-    "title" => 'Изменение убеждений с помощью НЛП',
-    "pages" => 256
-)
-];
+    //* - Отсортируйте массив по возрастанию дат
+    array_multisort($date,SORT_ASC);
+    echo "<br>Отсортировано по возрастанию:";
+    var_dump($date);
 
- echo "Недавно я прочитал книги \"".$books[0]['title']."\" и \"".$books[1]['title']."\",
-   написанные соответственно авторами ".$books[0]['author']." и ".$books[1]['author'].", я осилил в сумме ".
-           ($books[0]['pages'] + $books[1]['pages'])." страниц, не ожидал от себя.";
+    // * - С помощью функция для работы с массивами извлеките последний элемент массива в новую переменную $selected
+    $selected = array_pop($date);
+    echo "Вытолкнут последний элемент:";
+    var_dump($date);
 
+    // * - C помощью функции date() выведите $selected на экран в формате "дд.мм.ГГ ЧЧ:ММ:СС"
+    echo "Вывод конвертированной даты вытолкнутого элемента:<br>";
+    echo date('d-m-Y h:m:s', $selected);
+
+    // * - Выставьте часовой пояс для Нью-Йорка, и сделайте вывод снова, чтобы проверить, что часовой пояс был изменен успешно
+    // */
+    echo "<br>Вывод конвертированной даты вытолкнутого элемента по времени Нью-Йорка (-8):<br>";
+    date_default_timezone_set('America/New_York');
+    echo date('d-m-Y h:m:s', $selected);
 ?>
